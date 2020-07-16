50968785bd00375af73235aa7d0351253ad03313
Cingala David <you@example.com>
2020-07-16 13:32:54 +0200

    wip

--- test.js
+++ test.js
@@ -1,17 +1,3 @@
-const calibers = require('./dataset/calibers3.json');
-const uniqBy = require('lodash/uniqBy');
-const fs = require('fs');
+const all = require('./dataset/all.json');
 
-const c = calibers.map(x => ({...x, id: `${x.brand_id}-${x.id}`}));
-const cal = uniqBy(c, 'id');
-
-console.log('ca', cal.length);
-
-// const f = brandsCalibers.filter(x => {
-//   const exist = !!brands.find(y => y.id === x.id);
-//   return !exist;
-// });
-
-fs.writeFile('./dataset/calibers4.json', JSON.stringify(cal), () => {
-  console.log('done');
-});
+console.log('all length', all.length);
